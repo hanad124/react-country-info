cc7a0f17d05c597af93a525d9813abb16e2292b6
Hanad Mohamed <hanaddaahir124@gmail.com>
2023-05-15 18:05:23 +0300

    Added Loading Skeleton

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,7 +8,7 @@ import "../Container/Container.scss";
 
 const Home = () => {
   const { region, setRegion } = useContext(RegionContext);
-  const [load, setLoad] = useState(false);
+  const [load, setLoad] = useState(true);
   const [countries, setCountries] = useState([]);
   const [filteredCountry, setFilteredCountry] = useState(countries);
   const [searchField, setSearchField] = useState("");
@@ -16,18 +16,16 @@ const Home = () => {
   const f = new Intl.NumberFormat("en-us");
 
   useEffect(() => {
+    setLoad(true);
     fetch("https://restcountries.com/v3.1/all")
       .then((response) => response.json())
       .then((Data) => {
-        console.log(Data);
         if (Data) {
-          setLoad(true);
+          setLoad(false);
         }
         setCountries(Data);
       });
   }, []);
-
-  console.log(load);
 
   useEffect(() => {
     const newFilteredCountry = countries.filter((country) => {
@@ -49,59 +47,59 @@ const Home = () => {
     }
   }, [region]);
 
-  return (
-    <div className="Home">
-      <div className="sub_header" onClick={() => {}}>
-        <Search changeHandler={onSearchChange} />
-        <Filter />
+    return (
+      <div className="Home">
+        <div className="sub_header" onClick={() => {}}>
+          <Search changeHandler={onSearchChange} />
+          <Filter />
+        </div>
+        <div className="cardsContainer">
+          {filteredCountry.map((count) => {
+            return (
+              <div
+                className={`card ${load ? "skeleton" : ""}`}
+                onClick={() => {
+                  console.log(count);
+                  localStorage.setItem("items", JSON.stringify(count));
+                }}>
+                <Link to="/country">
+                  <img
+                    src={count.flags.png}
+                    alt=""
+                    className={`flag ${load ? "skeleton" : ""}`}
+                  />
+                  <div
+                    className={`country-desc {
+   ${load ? "skeleton" : ""}`}>
+                    <div
+                      className={`country_name {
+   ${load ? "skeleton" : ""}`}>
+                      {count.name.common}
+                    </div>
+                    <div
+                      className={`population {
+   ${load ? "skeleton" : ""}`}>
+                      Population: <span>{f.format(count.population)}</span>
+                    </div>
+                    <div
+                      className={`region {
+   ${load ? "skeleton" : ""}`}>
+                      Region: <span>{count.region}</span>
+                    </div>
+                    <div
+                      className={`capital {
+   ${load ? "skeleton" : ""}`}>
+                      Capital: <span>{count.capital}</span>
+                    </div>
+                  </div>
+                </Link>
+              </div>
+            );
+          })}
+        </div>
+        <Outlet />
       </div>
-      <div className="cardsContainer">
-        {filteredCountry.map((count) => {
-          return (
-            <div
-              className={`card ${!load ? skeleton : ""}`}
-              onClick={() => {
-                console.log(count);
-                localStorage.setItem("items", JSON.stringify(count));
-              }}>
-              <Link to="/country">
-                <img
-                  src={count.flags.png}
-                  alt=""
-                  className={`flag ${!load ? skeleton : ""}`}
-                />
-                <div
-                  className={`country-desc {
- ${!load ? skeleton : ""}`}>
-                  <div
-                    className={`country_name {
- ${!load ? skeleton : ""}`}>
-                    {count.name.common}
-                  </div>
-                  <div
-                    className={`population {
- ${!load ? skeleton : ""}`}>
-                    Population: <span>{f.format(count.population)}</span>
-                  </div>
-                  <div
-                    className={`region {
- ${!load ? skeleton : ""}`}>
-                    Region: <span>{count.region}</span>
-                  </div>
-                  <div
-                    className={`capital {
- ${!load ? skeleton : ""}`}>
-                    Capital: <span>{count.capital}</span>
-                  </div>
-                </div>
-              </Link>
-            </div>
-          );
-        })}
-      </div>
-      <Outlet />
-    </div>
-  );
-};
+    );
+  }
 
 export default Home;
